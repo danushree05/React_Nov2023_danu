df487b2c41508f5fd1904df395b2c15e82e02f08
danushree05 <116280457+danushree05@users.noreply.github.com>
2023-11-16 21:18:18 +0530

    DAY2

---
+++ DAY2/reactapp2/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Aboutus from "./components/Aboutus";
+
+function App() {
+  return (
+    <div className="App">
+      <Aboutus/>
+    </div>
+  )
+}
+
+export default App;
---
+++ DAY2/reactapp2/src/components/Aboutus.js
@@ -0,0 +1,15 @@
+import "./Aboutus.css";
+const Aboutus = () =>{
+    return (
+        <div class="a">
+            <h1>About us</h1>
+            <hr></hr>
+            <p >
+            React is a JavaScript library for building user interfaces.
+            React is used to build single-page applications.
+            React allows us to create reusable UI components.
+            </p>
+        </div>
+    );
+};
+export default Aboutus;
