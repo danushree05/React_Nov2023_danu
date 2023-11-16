554a49df18088572fc9592dfcdbb4f7e5ab6aa3d
danushree05 <116280457+danushree05@users.noreply.github.com>
2023-11-16 23:52:36 +0530

    DAY4

---
+++ DAY4/reactapp1/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Listitem from "./components/Listitem";
+function App() {
+  return (
+    <div className="App">
+      <Listitem />
+    </div>
+  );
+}
+
+export default App;
